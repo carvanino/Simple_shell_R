c667c7690cf97fa14d964d2f1608545758b25079
Akinola <akinolatofunmi04@gmail.com>
2022-08-02 07:47:28 -0700

    Forks, wait and execute a process

---
+++ copy_of_fork+wait+execve.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * main - fork(),wait(),execve()
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *argv[] = {"/bin/ls", "-l", "/tmp/", NULL};
+	int status;
+	int proc;
+	int i = 0;
+	pid_t my_pid;
+	pid_t child_pid = 1;
+
+	printf("Begining of the process\n");
+	my_pid == getpid();
+	printf("This is the parent process with ID: %u\n", my_pid);
+	while (i < 5 && child_pid != 0)
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error:");
+			return (1);
+		}
+		printf("Still in parent, see my ID: %u do I have child process yet? ID: %u\n", my_pid, child_pid);
+		wait(&status);
+		i++;
+	}
+	if (child_pid == 0)
+	{
+		printf("Now, we have a child process with ID: %u and parent ID: %u\n\n", child_pid, my_pid);
+	}
+	else
+	{
+		printf("This is the father of all the processes with ID: %u\n\n", my_pid);
+	}
+
+	proc = execve(argv[0], argv, NULL);
+	if (proc == -1)
+	{
+		perror("Error:");
+		return (1);
+	}
+	return (0);
+}
